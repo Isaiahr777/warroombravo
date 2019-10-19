57525c02394c5f2c33240a5bd509ec2e03858847
Kali <kmccain@stu.jsu.edu>
2019-10-19 13:30:12 -0500

    finished TASDatabase

--- src/edu/jsu/mcis/tas_fa19/TASDatabase.java
+++ src/edu/jsu/mcis/tas_fa19/TASDatabase.java
@@ -3,6 +3,7 @@ package edu.jsu.mcis.tas_fa19;
 import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.time.LocalTime;
 
 public class TASDatabase {
     
@@ -48,6 +49,109 @@ public class TASDatabase {
         
         return B;
     }
+     public Shift getShift(String shift)throws SQLException{
+         STATE = conn.createStatement();
+         qShifted = "select from shift where id = '"+ shift +"'";
+         ResultSet RS = STATE.executeQuery(qShifted);
+         
+         int shiftID = 0;
+         int lunchDuration = 0;
+         String shiftDescription = null;
+         LocalTime start= null;
+         LocalTime stop = null;
+         int interval=0;
+         int gracePeriod=0;
+         int dock=0;
+         LocalTime lunchStart = null;
+         LocalTime lunchStop = null;
+         int lunchDeduct=0;
+         
+         while (RS.next()){
+             shiftID = RS.getInt("id");
+             shiftDescription = RS.getString("description");
+             start = (LocalTime) RS.getObject("start");
+             stop = (LocalTime) RS.getObject("stop");
+             interval = RS.getInt("interval");
+             gracePeriod = RS.getInt("graceperiod");
+             dock = RS.getInt("dock");
+             lunchStart = (LocalTime) RS.getObject("lunchstart");
+             lunchStop = (LocalTime) RS.getObject("lunchstop");
+             lunchDeduct = RS.getInt("lunchdeduct");
+             
+         }
+         close(STATE);
+         
+         Shift shifts = new Shift(lunchDuration, shiftID, shiftDescription, start, stop,
+         interval, gracePeriod, dock, lunchStart, lunchStop, lunchDeduct);
+         
+         return shifts;
+     }
+     public Shift getShift(Badge badge) throws SQLException{
+         STATE = conn.createStatement();
+         qShifted = "select from shift where id = '"+ badge +"'";
+         ResultSet RS = STATE.executeQuery(qShifted);
+         
+         int shiftID = 0;
+         int lunchDuration = 0;
+         String shiftDescription = null;
+         LocalTime start= null;
+         LocalTime stop = null;
+         int interval=0;
+         int gracePeriod=0;
+         int dock=0;
+         LocalTime lunchStart = null;
+         LocalTime lunchStop = null;
+         int lunchDeduct=0;
+         
+         while (RS.next()){
+             shiftID = RS.getInt("id");
+             shiftDescription = RS.getString("description");
+             start = (LocalTime) RS.getObject("start");
+             stop = (LocalTime) RS.getObject("stop");
+             interval = RS.getInt("interval");
+             gracePeriod = RS.getInt("graceperiod");
+             dock = RS.getInt("dock");
+             lunchStart = (LocalTime) RS.getObject("lunchstart");
+             lunchStop = (LocalTime) RS.getObject("lunchstop");
+             lunchDeduct = RS.getInt("lunchdeduct");
+             
+         }
+         close(STATE);
+         
+         Shift shifts = new Shift(lunchDuration, shiftID, shiftDescription, start, stop,
+         interval, gracePeriod, dock, lunchStart, lunchStop, lunchDeduct);
+         
+         return shifts;
+     }
+     public Punch getPunch(String punch) throws SQLException{
+          STATE = conn.createStatement();
+         qShifted = "select from shift where id = '"+ punch +"'";
+         ResultSet RS = STATE.executeQuery(qShifted);
+         
+         Badge badge = null;
+         int punchID = 0;
+         int terminalId = 0;
+         String badgeID = null;
+         long originalTimeStamp = 0;
+         int punchTypeID = 0;
+         String adjustmentType = null;
+         
+         while (RS.next()){
+             punchID = RS.getInt("id");
+             terminalId = RS.getInt("terminalid");
+             badgeID = RS.getString("badgeid");
+             originalTimeStamp = RS.getLong("originaltimestamp");
+             punchTypeID = RS.getInt("punchtypeid");
+             
+         }
+         close(STATE);
+         
+         Punch P = new Punch (badge, punchID, terminalId, badgeID, originalTimeStamp,
+         punchTypeID, adjustmentType);
+         
+         return P;
+         
+     }
      
     
     public void close(Statement STATE){
